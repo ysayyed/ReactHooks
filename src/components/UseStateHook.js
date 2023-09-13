474a443f061468fae77224399c344a601317d8aa
ysayyed <muteweb.yaseen@gmail.com>
2023-09-13 23:57:22 +0530

    First Commit

---
+++ src/components/UseStateHook.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+const Input = styled.input`
+	height: 30px;
+	width: 200px;
+	margin: 20px 0 0 20px;
+
+	&:focus{
+		background-color: aliceblue;
+	}
+`;
+
+const Paragraph = styled.p`
+	margin: 20px 0 0 20px;
+`
+
+export default function UseStateHook() {
+	const [name, setName] = useState("")
+	function nameHandler(event) {
+		setName(event.target.value)
+	}
+	return (
+		<div>
+			{/* <input type="text" onChange={nameHandler} placeholder='Enter your name' /> */}
+			<Input onChange={nameHandler} placeholder='Enter your name' />
+			<Paragraph>{name}</Paragraph>
+		</div>
+	)
+}
