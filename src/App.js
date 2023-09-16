e77f8a451ecd82158497c164a00f7cce75cb6d58
ysayyed <muteweb.yaseen@gmail.com>
2023-09-16 06:45:04 +0530

    Feat: Addition in useState

--- src/App.js
+++ src/App.js
@@ -1,44 +1,55 @@
 import { useState } from 'react'
 import styled from 'styled-components'
-import UseStateHook from '../src/components/useStateExamples/UseStateHook'  
+import UseStateHook from '../src/components/useStateExamples/UseStateHook'
 import UseStateHookChildToParent from './components/useStateExamples/UseStateHookChildToParent'
 import UseStateHookPrevValue from './components/useStateExamples/UseStateHookPrevValue'
+import UseStateHookArray from './components/useStateExamples/UseStateHookArray'
 
 const Paragraph = styled.p`
 	margin: 20px 0 0 20px;
 `;
 
 function App() {
-  
-  // ***************************************************************
-  // Uncomment following return block to use UseStateHook component:
-  // ***************************************************************
-  
-  // return (
-  //     <div >
-  //       <UseStateHook/>
-  //     </div>
-  //   );
-  
-  // ****************************************************************************
-  // Uncomment following return block to use UseStateHookChildToParent component:
-  // ****************************************************************************
-  
-    // const [name, setName] = useState("")
-    // const useChildData = (name)=>{
-    //   setName(name)
-    // }
-    // return (
-    //   <div >
-    //     <UseStateHookChildToParent sendData ={useChildData}/>
-    //     <Paragraph>{name}</Paragraph>
-    //   </div>
-    // );
-
-    return(
-      <UseStateHookPrevValue/>
-    )
-    
+  return (
+    <>
+      <UseStateHookArray />
+    </>
+  )
 }
 
 export default App;
+
+
+// ***************************************************************
+// Uncomment following return block to use UseStateHook component:
+// ***************************************************************
+
+// return (
+//     <div >
+//       <UseStateHook/>
+//     </div>
+//   );
+
+// ****************************************************************************
+// Uncomment following return block to use UseStateHookChildToParent component:
+// ****************************************************************************
+
+// const [name, setName] = useState("")
+// const useChildData = (name)=>{
+//   setName(name)
+// }
+// return (
+//   <div >
+//     <UseStateHookChildToParent sendData ={useChildData}/>
+//     <Paragraph>{name}</Paragraph>
+//   </div>
+// );
+
+// ****************************************************************************
+// Uncomment following return block to use UseStateHookPrevValue component:
+// ****************************************************************************
+
+
+// return (
+//   <UseStateHookPrevValue />
+// )
---
+++ src/components/useStateExamples/UseStateHookArray.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const Input = styled.input`
+	height: 30px;
+	width: 200px;
+	margin: 20px 0 0 20px;
+
+	&:focus{
+		background-color: aliceblue;
+	}
+`;
+
+const Code = styled.code`
+    background: whitesmoke;
+    padding: 7px;
+    color: crimson;
+    font-weight: bolder;
+    border-radius: 10px;
+`
+const Paragraph = styled.p`
+	margin: 20px 0 0 20px;
+    font-weight: bold;
+`
+
+const Button = styled.button`
+	margin: 20px 0 0 20px;
+    font-weight: bold;
+`
+
+
+export default function UseStateHookArray() {
+	const [array, setArray] = useState([])
+	const [val, setVal] = useState()
+
+	function inputHandler(event) {
+		setVal(event.target.value)
+	}
+	function arrayHandler() {
+		setArray([...array, val])
+	}
+	return (
+		<>
+			<Paragraph>Use of <Code>useState</Code> hook for arrays.</Paragraph>
+			<Paragraph># Updating array by pushing recent input values. </Paragraph>
+			<Paragraph># Also watch the dynamic list in action.</Paragraph>
+			<Input type="text" onChange={inputHandler} />
+			<br />
+			<Button onClick={arrayHandler}>Submit</Button> <br />
+			<ul>
+				{array.length > 0 ? array.map(item => <li>{item}</li>) : <li>None</li>}
+			</ul>
+		</>
+	)
+}
